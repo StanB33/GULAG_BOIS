81b2c262d5b622572923c622f601bd80c6c1a725
Thibaux <38690826+Thibaux@users.noreply.github.com>
2018-04-24 13:44:39 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,79 @@
+<!--
+/**
+ * @author Thibaux Swildens <s1114354@student.windesheim.nl>
+ * @link https://s1114354-php.clow.nl
+ */
+-->
+
+
+<!DOCTYPE HTML>
+<html lang="nl">
+<head>
+    <title>Thibaux's pagina</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway'>
+    <link rel="stylesheet" href="../CSS/style_clow.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="omschrijving" content="Homepage voor Thibaux">
+    <meta name="keywords" content="HTML, PHP, CSS">
+    <meta name="author" content="Thibaux Swildens">
+</head>
+<body>
+<header>
+    <a href="https://s1114354-php.clow.nl" class="header-naam">Thibaux Swildens</a>
+    <nav>
+        <ul>
+            <li><a target="_blank" href="https://s1114354-php.clow.nl/periode_1.html">Periode 1</a></li>
+            <li><a target="_blank" href="https://s1114354-php.clow.nl/periode_2.html">Periode 2</a></li>
+            <li><a target="_blank" href="https://s1114354-php.clow.nl/periode_3.html">Periode 3</a></li>
+            <li><a target="_blank" href="https://s1114354-php.clow.nl/periode_4.html">Periode 4</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="index-banner">
+        <div class="vertical-center">
+            <h2>HBO-ICT Student</h2>
+            <h3>Eerstejaars HBO-ICT student aan het Windesheim Flevoland te Almere</h3>
+        </div>
+    </section>
+    <div class="wrapper">
+        <div class="index-uitgelicht">
+            <h2>Uitgelicht:</h2>
+        </div>
+        <section class="index-links">
+            <a href="https://s1114354-php.clow.nl/quiz/landingspagina.html">
+                <div class="index-box-1">
+                    <h3>Quiz - Multimedia & Design</h3>
+                </div>
+            </a>
+            <a href="https://s1114354-php.clow.nl/whatstudy/inlogform.html">
+                <div class="index-box-2">
+                    <h3>Whatstudy</h3>
+                </div>
+            </a>
+            <a href="https://s1114354-php.clow.nl/webshop/index.php?controller=home&action=welcome">
+                <div class="index-box-3">
+                    <h3>Webshop</h3>
+                </div>
+            </a>
+            <a href="https://s1114354-php.clow.nl/wordpress/index.php">
+                <div class="index-box-4">
+                    <h3>Wordpress</h3>
+                </div>
+            </a>
+        </section>
+</main>
+</div>
+<footer>
+    <div class="footer-foto">
+        <img src="../Img/2jsbryjusg2y.png">
+    </div>
+    <ul class="footer-email">
+        <li>Email: <a href="mailto:s1114354@student.windesheim.nl?Subject=Zegt%20u%20het%20maar?" target="_top">s1114354@student.windesheim.nl</a></li>
+    </ul>
+    <br>
+    <div class="footer-©">&copy;Thibaux Swildens</div>
+</footer>
+</body>
+</html>
